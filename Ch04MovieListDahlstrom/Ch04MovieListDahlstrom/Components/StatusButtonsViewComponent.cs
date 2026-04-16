03c139542c967608b3747bb223e49d335d82390d
Holly Dahlstrom <hmb0831@yahoo.com>
2026-04-15 20:17:50 -0500

    Module 13 Assignment: Website Add Tag Helper & View Components

---
+++ Ch04MovieListDahlstrom/Ch04MovieListDahlstrom/Components/StatusButtonsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ch04MovieListDahlstrom.Models;
+
+namespace Ch04MovieListDahlstrom.Components
+{
+    public class StatusButtonsViewComponent : ViewComponent
+    {
+        private readonly TicketContext _context;
+
+        public StatusButtonsViewComponent(TicketContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var statuses = _context.Statuses.ToList();
+            return View(statuses);
+        }
+    }
+}
---
+++ Ch04MovieListDahlstrom/Ch04MovieListDahlstrom/TagHelpers/SubmitButtonTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Ch04MovieListDahlstrom.TagHelpers
+{
+    [HtmlTargetElement("submit-button")]
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public string Text { get; set; } = "Submit";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.Attributes.SetAttribute("type", "submit");
+            output.Attributes.SetAttribute("class", "btn btn-primary");
+
+            output.Content.SetContent(Text);
+        }
+    }
+}
